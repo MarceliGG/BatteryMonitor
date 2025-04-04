4d9e4051939fc9ecb552bab68f1ba8dfccee1ae8
MarceliGG <marcel.giemza@outlook.com>
2025-04-04 15:46:50 +0200

    low battery notifications

--- app/src/main/java/pl/marcelg/batterymanager/MainActivity.kt
+++ app/src/main/java/pl/marcelg/batterymanager/MainActivity.kt
@@ -1,10 +1,15 @@
 package pl.marcelg.batterymanager
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.os.BatteryManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +17,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -25,18 +33,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import pl.marcelg.batterymanager.ui.theme.BatteryMonitorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createNotificationChannel(this)
+
         enableEdgeToEdge()
         setContent {
             BatteryMonitorTheme {
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.fillMaxSize().statusBarsPadding(), horizontalAlignment = Alignment.CenterHorizontally) {
-                        BatteryPercentageDisplay(this@MainActivity)
-                    }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    BatteryPercentageDisplay(this@MainActivity)
+                }
 //                }
             }
         }
@@ -65,13 +84,55 @@ fun BatteryPercentageDisplay(context: Context) {
     Text(text = if (batteryChargerState) "Charging" else "Not charging", fontSize = 18.sp)
 }
 
-class BatteryReceiver(private val onBatteryLevelChanged: (Int, Boolean) -> Unit) : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+const val CHANNEL_ID = "general"
+const val LOW_ID = 0
+
+fun createNotificationChannel(context: Context) {
+    val name = "General"
+    val descriptionText = "Main channel"
+    val importance = NotificationManager.IMPORTANCE_DEFAULT
+    val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+    mChannel.description = descriptionText
+    // Register the channel with the system. You can't change the importance
+    // or other notification behaviors after this.
+    val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager;
+    notificationManager.createNotificationChannel(mChannel)
+}
+
+class BatteryReceiver(private val onBatteryLevelChanged: (Int, Boolean) -> Unit) :
+    BroadcastReceiver() {
+    private var notifLowSent = false;
+
+    override fun onReceive(context: Context, intent: Intent?) {
         val level: Int = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
         val scale: Int = intent?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
         val charging: Int = intent?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) ?: -1
         if (level != -1 && scale != -1) {
             val batteryPct = (level / scale.toFloat() * 100).toInt()
+            if (batteryPct <= 20) {
+                if (!notifLowSent) {
+
+//                    context?.let {
+                    var builder = NotificationCompat.Builder(
+                        context,
+                        CHANNEL_ID
+                    ).setSmallIcon(R.drawable.ic_stat_battery_alert)
+                        .setContentTitle("Low Battery!!!!!!")
+                        .setContentText("Battery fell to 20%!!!!!")
+                    with(NotificationManagerCompat.from(context)) {
+                        notify(LOW_ID, builder.build())
+                    }
+//                    }
+
+                    println("low");
+                    notifLowSent = true;
+                }
+            } else {
+                with(NotificationManagerCompat.from(context)) {
+                    cancel(LOW_ID)
+                }
+                notifLowSent = false;
+            }
             onBatteryLevelChanged(batteryPct, charging != 0)
         }
     }
